fad82baad29edcffe27c5659662c230da97bab3b
Julian Arenas Lopez <jarenasl@unal.edu.co>
2021-10-26 11:24:26 -0500

    Se cambia el server

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env('HOST', 'htttps://buzondenavidad.com'),
+  port: env.int('PORT', 1338),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'a79b314a95b2bbdd42f088e9db485e19'),
