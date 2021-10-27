9f71e9b43851da9b05fd098133ca939934803891
Julian Arenas Lopez <jarenasl@unal.edu.co>
2021-10-27 13:10:02 -0500

    Se agrega url y se pone order a los age_groups

--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', 'htttps://buzondenavidad.com'),
-  port: env.int('PORT', 1338),
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  url: env('SERVER_URL', 'https://buzondenavidad.com:1338'),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'a79b314a95b2bbdd42f088e9db485e19'),
